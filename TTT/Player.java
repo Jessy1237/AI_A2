621174c6a9d21fd570c45a479fe0ab273cffc60e
Jessy1237 <j.e.s.s.e.b.o.yvs@gmail.com>
2018-09-23 17:47:24 +0200

    Trying alphaBeta pruning
    gets kattis score of 2

--- TTT/Player.java
+++ TTT/Player.java
@@ -28,14 +28,51 @@ public class Player
         //return nextStates.elementAt(random.nextInt(nextStates.size()));
 
         GameState bestState = null;
-        int bestVal = 0;
-        for ( GameState state : nextStates )
+        //int bestVal = 0;
+        int beta = Integer.MAX_VALUE;
+        int alpha = Integer.MIN_VALUE;
+        int depth = 5;
+
+        if ( gameState.getNextPlayer() == Constants.CELL_O ) // Player A i.e. its the other players turn next
         {
-            int tempVal = miniMax( state, gameState.getNextPlayer() );
-            if ( tempVal > bestVal )
+            int v = Integer.MIN_VALUE;
+            for ( GameState state : nextStates )
             {
-                bestVal = tempVal;
-                bestState = state;
+                //    int tempVal = miniMax( state, gameState.getNextPlayer() );
+                //    if ( tempVal > bestVal )
+                //    {
+                //        bestVal = tempVal;
+                //        bestState = state;
+                //    }
+                int temp = alphaBeta( state, depth - 1, alpha, beta, gameState.getNextPlayer() );
+                if ( temp > v ) //equiv to Math.max but also gives us the best state
+                {
+                    v = temp;
+                    bestState = state;
+                }
+
+                alpha = Math.max( alpha, v );
+
+                if ( beta <= alpha )
+                    break;
+            }
+        }
+        else //Player B
+        {
+            int v = Integer.MAX_VALUE;
+            for ( GameState state : nextStates )
+            {
+                int temp = alphaBeta( state, depth - 1, alpha, beta, gameState.getNextPlayer() );
+                if ( temp < v ) //equiv to Math.min but also gives us the best state
+                {
+                    v = temp;
+                    bestState = state;
+                }
+
+                alpha = Math.min( alpha, v );
+
+                if ( beta <= alpha )
+                    break;
             }
         }
 
@@ -102,10 +139,10 @@ public class Player
                 int best = Integer.MIN_VALUE;
                 for ( GameState nextState : nextStates )
                 {
-                    int v = miniMax(nextState, state.getNextPlayer());
+                    int v = miniMax( nextState, state.getNextPlayer() );
                     best = Math.max( best, v );
                 }
-                
+
                 return best;
             }
             else //player B
@@ -113,12 +150,48 @@ public class Player
                 int best = Integer.MAX_VALUE;
                 for ( GameState nextState : nextStates )
                 {
-                    int v = miniMax(nextState, state.getNextPlayer());
+                    int v = miniMax( nextState, state.getNextPlayer() );
                     best = Math.min( best, v );
                 }
-                
+
                 return best;
             }
         }
+    }
+
+    private int alphaBeta( GameState state, int depth, int alpha, int beta, int player )
+    {
+        Vector<GameState> nextStates = new Vector<GameState>();
+        state.findPossibleMoves( nextStates );
+
+        int v = Integer.MAX_VALUE; // setting V for player B
+
+        if ( nextStates.size() == 0 || depth == 0 ) //terminal state
+        {
+            return eval( state, player );
+        }
+        else if ( player == Constants.CELL_X ) //Player A
+        {
+            v = Integer.MIN_VALUE;
+            for ( GameState nextState : nextStates )
+            {
+                v = Math.max( v, alphaBeta( nextState, depth - 1, alpha, beta, state.getNextPlayer() ) );
+                alpha = Math.max( alpha, v );
+                if ( beta <= alpha )
+                    break; //Beta prune
+            }
+        }
+        else //Player B
+        {
+            for ( GameState nextState : nextStates )
+            {
+                v = Math.min( v, alphaBeta( nextState, depth - 1, alpha, beta, state.getNextPlayer() ) );
+                beta = Math.min( beta, v );
+                if ( beta <= alpha )
+                    break; //alpha prune
+            }
+        }
+
+        return v;
     }
 }
